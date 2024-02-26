28be320966c0fd68666ce2c142394469f83abe19
“Lukasz <lukasz.glowacz@glowacz.se>
2024-02-26 16:31:45 +0100

    Added React Bootstrap Module and test it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Button from 'react-bootstrap/Button';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>5th project App Code</h1>
+      <Button variant="success">Success</Button>
     </div>
   );
 }
