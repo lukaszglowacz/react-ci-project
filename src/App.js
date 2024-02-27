c0a2f757d18ef5673226ae344eaa6cd36fd0e8d8
“Lukasz <lukasz.glowacz@glowacz.se>
2024-02-27 15:54:28 +0100

    created useContextHook for logged in user and seperate navbar icons for logged in and not logged in users

--- src/App.js
+++ src/App.js
@@ -4,20 +4,45 @@ import { Container } from "react-bootstrap";
 import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
 import './api/axiosDefaults'
 import SignUpForm from './pages/auth/SignUpForm';
+import SignInForm from './pages/auth/SignInForm';
+import { createContext, useEffect, useState } from 'react';
+import axios from 'axios';
+
+export const currentUserContext = createContext();
+export const setCurrentUserContext = createContext();
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  const handleMount = async () => {
+    try {
+      const { data } = await axios.get('/dj-rest-auth/user/')
+      setCurrentUser(data);
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    handleMount();
+  }, [])
+
   return (
-    <div className={styles.App}>
-      <NavBar />
-      <Container className={styles.Main}>
-        <Switch>
-          <Route exact path="/" render={() => <h1>Home Page</h1>} />
-          <Route exact path="/signin" render={() => <h1>Sign In</h1>} />
-          <Route exact path="/signup" render={() => <SignUpForm />} />
-          <Route  render={() => <p>Page not found!</p>} />
-        </Switch>
-      </Container>
-    </div>
+    <currentUserContext.Provider value={currentUser}>
+      <setCurrentUserContext.Provider value={setCurrentUser}>
+        <div className={styles.App}>
+          <NavBar />
+          <Container className={styles.Main}>
+            <Switch>
+              <Route exact path="/" render={() => <h1>Home Page</h1>} />
+              <Route exact path="/signin" render={() => <SignInForm />} />
+              <Route exact path="/signup" render={() => <SignUpForm />} />
+              <Route render={() => <p>Page not found!</p>} />
+            </Switch>
+          </Container>
+        </div>
+      </setCurrentUserContext.Provider>
+    </currentUserContext.Provider>
   );
 }
 
--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://project-5-ci-db59797f355e.herokuapp.com/'
+axios.defaults.baseURL = 'http://127.0.0.1:8000'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true
