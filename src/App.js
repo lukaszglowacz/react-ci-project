73f8fdea0a9be317aab17c55f61f3de617ed53b4
“Lukasz <lukasz.glowacz@glowacz.se>
2024-02-27 23:28:15 +0100

    Deploy

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import styles from './App.module.css';
-import NavBar from './components/NavBar';
-import { Container } from "react-bootstrap";
-import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
-import './api/axiosDefaults'
-import SignUpForm from './pages/auth/SignUpForm';
-import SignInForm from './pages/auth/SignInForm';
-
+import styles from "./App.module.css";
+import NavBar from "./components/NavBar";
+import Container from "react-bootstrap/Container";
+import { Route, Switch } from "react-router-dom";
+import "./api/axiosDefaults";
+import SignUpForm from "./pages/auth/SignUpForm";
+import SignInForm from "./pages/auth/SignInForm";
+import PostCreateForm from "./pages/posts/PostCreateForm";
 
 function App() {
   return (
-    
-        <div className={styles.App}>
-          <NavBar />
-          <Container className={styles.Main}>
-            <Switch>
-              <Route exact path="/" render={() => <h1>Home Page</h1>} />
-              <Route exact path="/signin" render={() => <SignInForm />} />
-              <Route exact path="/signup" render={() => <SignUpForm />} />
-              <Route render={() => <p>Page not found!</p>} />
-            </Switch>
-          </Container>
-        </div>
-      
+    <div className={styles.App}>
+      <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home page</h1>} />
+          <Route exact path="/signin" render={() => <SignInForm />} />
+          <Route exact path="/signup" render={() => <SignUpForm />} />
+          <Route exact path="/posts/create" render={() => <PostCreateForm />} />
+          <Route render={() => <p>Page not found!</p>} />
+        </Switch>
+      </Container>
+    </div>
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -24,7 +24,7 @@ const NavBar = () => {
     }
 
     const addPostIcon = (
-        <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/post/create"><i className="far fa-plus-square"></i>Add post</NavLink>
+        <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/posts/create"><i className="far fa-plus-square"></i>Add post</NavLink>
     )
     const loggedInIcons = <>
 
