ad1bcd98f79128f0e1f1b6de60f3a031ad7319c8
“Lukasz <lukasz.glowacz@glowacz.se>
2024-02-27 12:57:39 +0100

    Added and style Signup Form

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = 'https://project-5-ci-db59797f355e.herokuapp.com/'
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
+axios.defaults.withCredentials = true
