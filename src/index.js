2634747da2e0a4f3c3556b63b0b9c7ace3cae6cd
“Lukasz <lukasz.glowacz@glowacz.se>
2024-02-29 11:35:42 +0100

    Created ProfilePage header and add follow unfollowe button to the ProfilePage header. Now user can see stats of each profiles and add profiles to followed unfollowed when he is logged in.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from 'react-router-dom/cjs/react-router-dom.min';
-import { CurrentUserProvider } from './contexts/CurrentUserContext';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router } from "react-router-dom";
+import { CurrentUserProvider } from "./contexts/CurrentUserContext";
+import { ProfileDataProvider } from "./contexts/ProfileDataContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <Router>
       <CurrentUserProvider>
-        <App />
+        <ProfileDataProvider>
+          <App />
+        </ProfileDataProvider>
       </CurrentUserProvider>
-
     </Router>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
